c0da3bc4fb257a50ef1936aaae48c4a83f29046e
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-04-09 02:43:06 +0300

    Finish!

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
 $( document ).ready(function() {
 
-	console.log( 'JQuery!!!' );
+	new WOW().init();
+
+	$('.photo__slider').slick({
+		dots: true,
+		arrows: true,
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+				}
+			},	
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					arrows: false,
+				}
+			}
+		]
+	});
 
 });
