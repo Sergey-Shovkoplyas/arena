86a5c88440e74ee6d039264db6c37b193e2ed12e
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-06-04 13:03:46 +0300

    Add pupup

--- app/js/common.js
+++ app/js/common.js
@@ -29,4 +29,36 @@ $( document ).ready(function() {
 		]
 	});
 
+	// Popap toggle  --------------------------------------
+
+	let $popap = $('.popap'); 
+	$('.btn').not('.btn.popap__btn').on('click', function(e) {
+		e.preventDefault();
+		$popap.toggleClass('active');
+	});
+	$('.popap__close').on('click', function() {
+		$popap.toggleClass('active');
+	});
+	$('.popap').on('click', function(e) {
+		if(e.target === this) {
+			$popap.removeClass('active');
+		}
+	});
+
+	// Sent form to email  --------------------------------------
+
+	$("#popapForm").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+			$popap.removeClass('active');
+		});
+		return false;
+	});
+
 });
